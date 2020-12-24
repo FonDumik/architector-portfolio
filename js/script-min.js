a0a4a0dc7f747d58619bb8d6e913343bc83c04e7
FonDumik <fondumik@yandex.ru>
2020-12-24 22:35:04 +0500

    Initial commit

---
+++ js/script-min.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $("a[href*=#]").bind("click", function (e) {
+    var anchor = $(this);
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $(anchor.attr("href")).offset().top - 50,
+        },
+        1000
+      );
+    e.preventDefault();
+  });
+  return false;
+});
+
+// console.info($(".test").val(""));
