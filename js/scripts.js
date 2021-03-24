89db1b95ada2af5bca191d993245787de0405966
Oleg <fondumik@yandex.ru>
2021-03-24 12:52:04 +0500

    animated progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   "use strict";
+
+  let progressVal = $('.progress-value');
+  progressVal.css( "display", "none"); 
+
   $("a[href*=#]").bind("click", function (e) {
     let anchor = $(this);
     $("html, body")
@@ -12,6 +16,24 @@ $(document).ready(function () {
       );
     e.preventDefault();
   });
+     
+  function scrollingToSkills() {
+    let counter = 0,
+        skills_item = $('.skills_item:first-child');
 
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop() + $(window).height();
+      var offset = skills_item.offset().top;
+    
+      if (scroll > offset && counter == 0) {
+        setTimeout(function() {
+          progressVal.css( "display", "block"); 
+          counter = 1;
+        }, 1500)
+      }
+    });
+  }
+  scrollingToSkills();
+  
   return false;
 });
